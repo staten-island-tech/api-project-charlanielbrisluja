ab51523947f9f0aa16409379ba46e36a3049e6ee
popplio2 <danikbrisk@yahoo.com>
2021-05-10 08:10:52 -0400

    Revert "ew"
    This reverts commit 58c74acdd359c97888a6cabc35a9815688fff238.

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,5 +1,5 @@
 const DOMSelectors = {
-    addButton: document.querySelectorAll(".addbtn"),
+    addButton: document.querySelector(".addbtn"),
     monsterCard: document.querySelector(".monster-card"),
     selectedBox: document.querySelector(".selected-box"),
     cardBox: document.querySelector(".card-box"),
